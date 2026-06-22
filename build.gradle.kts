3266215419cbf4c68c9c02b762387677082d88bd
Youssef Shoaib <canonballt@gmail.com>
2026-06-22 17:28:34 +0100

    Use Kotlin ABI validation

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.node.gradle) apply false
-    alias(libs.plugins.kotlin.binary.compatibility.validator) apply false
     alias(libs.plugins.buildconfig) apply false
 }
 
